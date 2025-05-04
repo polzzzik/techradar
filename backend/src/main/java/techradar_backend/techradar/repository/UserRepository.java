51d6ef2e1bc164324546960c97047c1a8a3716e1
Alina Polzikova <108518096+polzzzik@users.noreply.github.com>
2025-05-04 06:12:49 +0300

    Delete backend directory

--- backend/src/main/java/techradar_backend/techradar/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package techradar_backend.techradar.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import techradar_backend.techradar.entity.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
-
