a27bdd65ccd9c040596532e43e98105ba55ae797
polzzzik <polzikova.alina@icloud.com>
2025-06-19 10:55:55 +0300

    Удалены все файлы

--- backend/internal/storage/errors.go
+++
@@ -1,8 +0,0 @@
-package storage
-
-import "errors"
-
-var (
-	ErrAlreadyExists = errors.New("already exists")
-	ErrNotFound      = errors.New("not found")
-)
--- backend/internal/usecase/technology/delete_technology.go
+++
@@ -1,7 +0,0 @@
-package technology
-
-import "context"
-
-func (uc *UseCase) DeleteTechnologyByID(ctx context.Context, id int) error {
-	return uc.storage.DeleteTechnology(ctx, id)
-}
